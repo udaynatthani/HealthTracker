66aa2649fb5887063b4dfe98f478dde969f8549a
[uday natthani] <165016943+udaynatthani@users.noreply.github.com>
2026-02-24 18:01:59 +0530

    f

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 connectDB();
-
+app.use(cors({
+    origin: "*"
+  }));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
