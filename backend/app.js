bdf5b0fe4ec7bf6195d4443ca92e41fe369682c9
[uday natthani] <165016943+udaynatthani@users.noreply.github.com>
2026-02-24 18:25:59 +0530

    f

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+
+const app = express();   // ✅ create app first
+
 app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  origin: "*"
 }));
+
+app.use(express.json());
 
 const uploadRoutes = require("./routes/upload.routes");
 const analyzeRoutes = require("./routes/analyze.routes");
 const aiRoutes = require("./routes/ai.routes");
 const authRoutes = require("./routes/auth.routes");
 
-const app = express();
-
-
 app.use("/api", aiRoutes);
-app.use(express.json());
-app.use("/api",analyzeRoutes);
+app.use("/api", analyzeRoutes);
 app.use("/api", uploadRoutes);
 app.use("/api/auth", authRoutes);
 
-
 app.get("/health", (req, res) => {
-    res.status(200).json({
-      status: "Backend is running 🚀"
-    });
+  res.status(200).json({
+    status: "Backend is running 🚀"
   });
-  
-  
-
-
+});
 
 module.exports = app;
