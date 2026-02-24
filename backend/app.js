09ba7b0cde74e9787ccbc53064e31e27ec00bb4d
[uday natthani] <165016943+udaynatthani@users.noreply.github.com>
2026-02-24 18:23:31 +0530

    f

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,11 @@ const uploadRoutes = require("./routes/upload.routes");
 const analyzeRoutes = require("./routes/analyze.routes");
 const aiRoutes = require("./routes/ai.routes");
 const authRoutes = require("./routes/auth.routes");
-// const cors = require("cors");
+
 const app = express();
 
 
 app.use("/api", aiRoutes);
-app.use(cors());
 app.use(express.json());
 app.use("/api",analyzeRoutes);
 app.use("/api", uploadRoutes);
