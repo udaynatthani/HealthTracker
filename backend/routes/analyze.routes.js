6343cd2ee58166f99bd5e90d6c7c59ade5536cc5
[uday natthani] <165016943+udaynatthani@users.noreply.github.com>
2026-02-28 18:53:06 +0530

    ai

--- backend/routes/analyze.routes.js
+++ backend/routes/analyze.routes.js
@@ -1,22 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const auth = require("../middleware/auth.middleware");
-const WearableData = require("../models/WearableData");
-const { analyzeHealth } = require('../services/ai.service');
+const { analyzeHealthWithAI } = require("../services/ai.service");
+
 router.post("/analyze", auth, async (req, res) => {
-    try {
-      const data = await WearableData.find({ userId: req.user.id });
-      console.log("req.user.id:", req.user.id);
-      if (!data.length) {
-        return res.status(400).json({ error: "No data available for analysis" });
-      }
-  
-      const result = await analyzeHealth(data);
-      res.json(result);
-  
-    } catch (err) {
-      console.error("ANALYZE ERROR:", err.message);
-      res.status(500).json({ error: "Server error" });
+  try {
+    const data = await WearableData.find({ userId: req.user.id });
+
+    if (!data.length) {
+      return res.status(400).json({ error: "No data available for analysis" });
     }
-  });
-module.exports = router;
+
+    const result = await analyzeHealthWithAI(data);
+
+    res.json(result);
+  } catch (err) {
+    console.error("ANALYZE FULL ERROR:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
