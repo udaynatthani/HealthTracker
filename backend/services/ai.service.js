3e4c9beff4a68ef3c7a083d81faf6bd19b13d2a9
[uday natthani] <165016943+udaynatthani@users.noreply.github.com>
2026-02-28 19:30:37 +0530

    ai

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const analyzeHealthWithAI = async (healthData) => {
   const response = await axios.post(
-    "https://healthtracker-1-o89e.onrender.com/analyze",
+   process.env.AI_SERVICE_URL + "/analyze",
     healthData,
     { headers: { "Content-Type": "application/json" } }
   );
